a8c731d7bf614cd9f2a5f3571c5028b032c82ca2
Jenna Johnson <jenna@a2-ai.com>
2024-04-23 19:29:17 +0000

    comment edits in R

--- R/set_functions.R
+++ R/set_functions.R
@@ -3,12 +3,17 @@
 
 #' initialize devious
 #'
-#' @param storage_directory path to storage directory
-#' @param permissions optional argument to set linux file permissions (in octal format)
-#' @param group optional argument to set group of files stored
+#' @param storage_directory path to the desired storage directory
+#' @param permissions optional: linux file permissions to automatically set for files added to the storage directory (in octal format)
+#' @param group optional: group to automatically set for files added to the storage directory
 #'
+#' @examples
+#' \dontrun{
+#' dvs_init("/data/project_x") # would initialize the project's storage directory at /data/project_x
+#' dvs_init("/data/project_x", 777, "project_x_group") # would initialize the project's storage directory at /data/project_x and configure the linux permissions "777" and group "project_x_group" for all files added to the storage directory
+#' }
 #'
-#' @return nothing, or errors initializing
+#' @return nothing, or initializing errors
 #'
 #' @export
 dvs_init <- function(storage_directory, permissions = 664, group = "") {
@@ -28,8 +33,14 @@ clean_paths <- function(files) {
 
 #' add files to the storage directory
 #'
-#' @param files files to add to the storage directory
+#' @param files file paths or glob patterns to add to the storage directory
 #' @param message optional argument to add a message to future data frame rows associated with these files
+#'
+#' @examples
+#' \dontrun{
+#' dvs_add("data/derived/*.csv") # would add all csv files in data/derived to the initialized storage directory
+#'
+#' }
 #'
 #' @return a data frame with the states of success of added files
 #'
@@ -41,12 +52,11 @@ dvs_add <- function(files, message = "") {
   res <- dvs_add_impl(files, message, strict)
 
   list(successes = res[[1]], errors = res[[2]])
-
 } # dvs_add
 
 #' get files previously added to the storage directory
 #'
-#' @param files files to get from the storage directory
+#' @param files file paths or glob patterns to get from the storage directory
 #'
 #' @return a data frame with the states of success of retrieved files
 #'
@@ -60,9 +70,9 @@ dvs_get <- function(files) {
 
 #' get the status of any file previously added to the storage directory
 #'
-#' @param files optional argument, when specified, returns data frame with only these specified files
+#' @param files optional argument: when specified, returns data frame with only these specified file paths or glob patterns
 #'
-#' @return a data frame with the statuses of files previously added
+#' @return a data frame with the statuses of all previously added files
 #'
 #' @import purrr
 #'
