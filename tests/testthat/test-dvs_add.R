565bec3c29339add897d4f7b4954a48aad97ca4f
Lance Ferrer <lance@a2-ai.com>
2024-05-13 21:06:39 +0000

    add test for dvs_add two files glob pattern

--- tests/testthat/test-dvs_add.R
+++ tests/testthat/test-dvs_add.R
@@ -47,9 +47,9 @@ test_that("can add a single file", {
 
   expect_true(file.exists(file.path(dvs$stor_dir, first_two_of_hash, rest_of_hash)))
 
-  withr::with_dir(tempdir(), {
-    fs::dir_tree(all = TRUE)
-  })
+  # withr::with_dir(tempdir(), {
+  #   fs::dir_tree(all = TRUE)
+  # })
 
 
   # print("")
@@ -59,3 +59,69 @@ test_that("can add a single file", {
   # print(sprintf("data_derived_dir: %s", data_derived_dir))
 
 })
+
+test_that("can add multiple files - same directory", {
+  dvs <- create_project_and_initialize_real_repo("add_single_file", parent.frame())
+
+  # check that directories exist after dvs_init
+  expect_true(dir.exists(dvs$proj_dir))
+  expect_true(dir.exists(dvs$stor_dir))
+  expect_true(dir.exists(file.path(dvs$proj_dir, ".git")))
+
+  data_derived_dir <- file.path(tempdir(), "projects/add_single_file/data/derived")
+  fs::dir_create(data_derived_dir)
+
+  #check data directory exists
+  expect_true(dir.exists(data_derived_dir))
+
+  # create two data files for testing
+  pk_data_1 <- data.frame(
+    USUBJID = c(1, 1, 1),
+    NTFD = c(0.5, 1, 2),
+    DV = c(379.444, 560.613, 0)
+  )
+
+  pk_data_2 <- data.frame(
+    USUBJID = c(2, 2, 2),
+    NTFD = c(0.4, 2, 3),
+    DV = c(359.44, 540.213, 1)
+  )
+
+  pk_data_path_1 <- file.path(data_derived_dir, "pk_data_1.csv")
+  pk_data_path_2 <- file.path(data_derived_dir, "pk_data_2.csv")
+  write.csv(pk_data_1, pk_data_path_1)
+  write.csv(pk_data_2, pk_data_path_2)
+
+  # dvs_add
+  withr::with_dir(dvs$proj_dir, {
+    added_files <- dvs_add("data/derived/*", message = "finished pk data assembly")
+  })
+
+  # check that metadata file exists
+  dvs_file_path_1 <- file.path(dvs$proj_dir, "data/derived/pk_data_1.csv.dvs")
+  dvs_file_path_2 <- file.path(dvs$proj_dir, "data/derived/pk_data_2.csv.dvs")
+  dvs_json_1 <- jsonlite::fromJSON(dvs_file_path_1)
+  dvs_json_2 <- jsonlite::fromJSON(dvs_file_path_2)
+
+  expect_true(file.exists(dvs_file_path_1))
+  expect_true(file.exists(dvs_file_path_2))
+
+
+  #check that git ingore is created
+  expect_true(file.exists(file.path(dvs$proj_dir, "data/derived/.gitignore")))
+
+  # check that both files was added in the stor_dir, but no equality check
+
+  first_two_of_hash_1 <- substring(dvs_json_1$blake3_checksum, 1, 2)
+  first_two_of_hash_2 <- substring(dvs_json_2$blake3_checksum, 1, 2)
+  rest_of_hash_1 <- substring(dvs_json_1$blake3_checksum, 3)
+  rest_of_hash_2 <- substring(dvs_json_2$blake3_checksum, 3)
+
+  expect_true(file.exists(file.path(dvs$stor_dir, first_two_of_hash_1, rest_of_hash_1)))
+  expect_true(file.exists(file.path(dvs$stor_dir, first_two_of_hash_2, rest_of_hash_2)))
+
+  withr::with_dir(tempdir(), {
+    fs::dir_tree(all = TRUE)
+  })
+})
+
